ef8a0056ebdb408ee0992e3126315092e32b5e25
Alek Jarmov <alek.jarmov@gmail.com>
2023-04-26 02:27:06 +0200

    Improve home site design.

--- store/models.py
+++ store/models.py
@@ -3,10 +3,12 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    price = models.PositiveIntegerField()  # actually is >= 0
-    stock = models.IntegerField()
+    price = models.PositiveIntegerField(blank=False)  # actually is >= 0
+    stock = models.PositiveIntegerField(default=0)
     image_url = models.URLField(max_length=7919)
     video_url = models.URLField(max_length=7919, blank=True, default="")
+    copies_sold = models.PositiveIntegerField(default=0)
+    description = models.TextField(blank=True, default="")
 
     def __str__(self):
         return self.name
--- store/views.py
+++ store/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 from django.http import HttpResponse
 
 
 def home(request):
     context = dict()
+    # get the 6 most sold products
+
+    top_sellers = Product.objects.order_by('-copies_sold')[:8]
     context['active'] = 'home'
+    context['top_sellers'] = top_sellers
+    context['test'] = [1, 2, 3]
+    print(context)
     return render(request, "store/home.html", context)
