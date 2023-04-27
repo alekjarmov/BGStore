958afcc590b704ad08e3be8da8bfb158481918be
Alek Jarmov <alek.jarmov@gmail.com>
2023-04-27 14:25:29 +0200

    Add languages model

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
 from .models import Product
+from. models import Language
+
+class ModelAdmin(admin.ModelAdmin):
+    def save_related(self, request, form, formsets, change):
+        super(ModelAdmin, self).save_related(request, form, formsets, change)
+        language = Language.objects.get(code="en")
+        if form.instance.languages.count() == 0:
+            form.instance.languages.add(language)
+
 
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ModelAdmin)
+admin.site.register(Language, ModelAdmin)
--- store/models.py
+++ store/models.py
@@ -9,6 +9,29 @@ class Product(models.Model):
     video_url = models.URLField(max_length=7919, blank=True, default="")
     copies_sold = models.PositiveIntegerField(default=0)
     description = models.TextField(blank=True, default="")
+    languages = models.ManyToManyField("Language", blank=True, related_name="products")
+    # objects = ProductManager()
+
+    def __str__(self):
+        return self.name
+
+    def get_short_description(self):
+        return self.description.split("\n")[0]
+
+    def save(self, *args, **kwargs):
+        print("Saving product")
+        super().save(*args, **kwargs)
+        # check if we have 0 langugages
+        if self.languages.count() == 0:
+            # add english as default
+            print("Adding english as default")
+            self.languages.add(Language.objects.get(code="en"))
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10, blank=True, default="")
+    image_url = models.URLField(max_length=7919)
 
     def __str__(self):
         return self.name
