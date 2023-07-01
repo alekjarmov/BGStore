9ebec15dbe2a8aaa5867f3f7168dc66c359eca82
Alek Jarmov <alek.jarmov@gmail.com>
2023-07-01 21:26:38 +0200

    Populate database

--- BGStore/settings.py
+++ BGStore/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
+
 import environ
 
 env = environ.Env()
@@ -108,6 +110,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "data/")
+MEDIA_URL = '/data/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- store/models.py
+++ store/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    price = models.PositiveIntegerField(blank=False)  # actually is >= 0
+    price = models.FloatField() # actually is >= 0
     stock = models.PositiveIntegerField(default=0)
     image_url = models.URLField(max_length=7919)
     video_url = models.URLField(max_length=7919, blank=True, default="")
